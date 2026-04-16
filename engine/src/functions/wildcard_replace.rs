22fb252add63af1de239dd8dfd96ca55db5e5c0a
Molnar Botond <molnarbotond.eagle@gmail.com>
2026-04-17 00:56:40 +0200

    Add blank line between doc comment and derive in wildcard_replace.rs

--- engine/src/functions/wildcard_replace.rs
+++ engine/src/functions/wildcard_replace.rs
@@ -59,6 +59,7 @@ use std::iter;
 /// * If `replacement` is not valid UTF-8.
 /// * If the `wildcard_pattern` results in an invalid regular expression (e.g., `**`).
 ///
+
 #[derive(Debug, Default)]
 pub struct WildcardReplaceFunction {}
 
